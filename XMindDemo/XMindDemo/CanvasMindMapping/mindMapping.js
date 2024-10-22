3d4f3a1f0540ea569a0aeac52a4c62ad008517e1
NorthPoleCat <598114329@qq.com>
2024-10-22 08:48:36 +0800

    draw drag line

--- XMindDemo/XMindDemo/CanvasMindMapping/mindMapping.js
+++ XMindDemo/XMindDemo/CanvasMindMapping/mindMapping.js
@@ -360,6 +360,83 @@ function maxNodeHight(node) {
     return Math.max(node.height, node.childrenHeight);
 }
 
+//// Draggable
+
+let isDragging = false;
+let prevLine = null;
+let startNode = null;
+let endNode = null;
+
+let startX = 0;
+let startY = 0;
+let lastX = 0;
+let lastY = 0;
+
+let crossingNodes = [];
+
+function clearPreviousLine() {
+    if (prevLine) {
+        const { x1, y1, x2, y2 } = prevLine;
+        ctx.clearRect(x1 - 1, y1 - 1, x2 - x1 + 2, y2 - y1 + 2);
+        drawCrossingNode();
+    }
+}
+
+function drawLine(x1, y1, x2, y2) {
+    clearPreviousLine();
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    prevLine = { x1, y1, x2, y2 };
+}
+
+canvas.addEventListener('mousedown', function(event) {
+    hideContextMenu()
+    elementslist.forEach(element => {
+        if (ctx.isPointInPath(element[0], event.offsetX*PIXEL_RATIO, event.offsetY*PIXEL_RATIO)) {
+            const rect = canvas.getBoundingClientRect();
+            startX = event.clientX - rect.left;
+            startY = event.clientY - rect.top;
+            isDragging = true;
+            prevLine = null;
+            crossingNodes.push(element)
+        }
+    })
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        const rect = canvas.getBoundingClientRect();
+        lastX = e.clientX - rect.left;
+        lastY = e.clientY - rect.top;
+
+        drawLine(startX, startY, lastX, lastY); // 每次移动时只画最新的线条
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDragging = false;
+
+    elementslist.forEach(element => {
+        if (ctx.isPointInPath(element[0], event.offsetX*PIXEL_RATIO, event.offsetY*PIXEL_RATIO)) {
+            
+            //set drag end node here
+            crossingNodes.push(element)
+        }
+    })
+    
+    clearPreviousLine();
+    prevLine = null;
+});
+
+function drawCrossingNode() {
+    
+}
+
 
 
 
